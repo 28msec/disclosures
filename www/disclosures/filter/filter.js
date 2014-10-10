08c245ba0aab5fa39499b9f26e56d773d08739aa
Alexander-Kreutz <a@28.io>
2014-10-10 12:28:12 +0200

    filter page, fixes #19

--- www/disclosures/filter/filter.js
+++ www/disclosures/filter/filter.js
@@ -7,38 +7,15 @@ angular
     $scope.filter = DisclosuresAPI.filter;
     $scope.filterParameters = filterParameters;
 
-    $scope.selectEntity = function(entity) {
-        $scope.filter.entities.push(entity);
+    $scope.recentYears = filterParameters.years.slice(0,4);
+    $scope.allYears = false;
+
+    $scope.showAllYears = function() {
+        $scope.allYears = true;
     };
 
-    $scope.searchEntity = function() {
-        var matches = [];
-        var searchTerm = $scope.filter.searchEntity.toLowerCase();
-        if(searchTerm !== '') {
-            filterParameters.entities.forEach(function(entity){
-                if(entity.name.toLowerCase().indexOf(searchTerm) !== -1) {
-                    matches.push(entity);
-                }
-            });
-        }
-        $scope.entitiesCompletion = matches;
-    };
-    
-    $scope.isSelected = function (list,item) {
-    	return list.indexOf(item) > -1;
-    };
-    
     $scope.$watch('filter', $scope.filterChange, true);
-    
-    $scope.toggleSelection = function (list,item) {
-        var idx = list.indexOf(item);
-       
-        if (idx > -1) {
-          list.splice(idx, 1);
-        } else {
-          list.push(item);
-        }
-    };
+
    
 })
 ;
