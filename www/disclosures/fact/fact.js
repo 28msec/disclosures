f77062a786b2238f7314ed0ad993a4c85a258e79
Alexander-Kreutz <a@28.io>
2014-10-21 15:47:17 +0200

    company name fix

--- www/disclosures/fact/fact.js
+++ www/disclosures/fact/fact.js
@@ -10,15 +10,14 @@ angular
     $scope.nav.fiscalPeriod = $stateParams.fiscalPeriod;
     $scope.nav.tag = $stateParams.tag;
     $scope.nav.page = 'fact';
+    $scope.nav.company = $stateParams.company;
     if (fact.FactTable && fact.FactTable.length > 0) {
 	    $scope.factValue = $sce.trustAsHtml(fact.FactTable[0].Value);
-	    $scope.factEntity = fact.FactTable[0].EntityRegistrantName;
+	    $scope.factEntity = $stateParams.company;
 	    $scope.factFiscalYear = fact.FactTable[0].Aspects['sec:FiscalYear'];
 	    $scope.factFiscalPeriod = fact.FactTable[0].Aspects['sec:FiscalPeriod'];
-        $scope.nav.company = fact.FactTable[0].EntityRegistrantName;
         $scope.missing = false;
     } else {
-        $scope.nav.company = $stateParams.aid;
     	$scope.missing = true;
     }
 });
