2c5f72e6efe082213683026dffb3dedaaa86f71c
William Candillon <wcandillon@gmail.com>
2014-10-22 19:02:31 +0200

    Improve logging

--- tasks/s3.js
+++ tasks/s3.js
@@ -1,9 +1,11 @@
 'use strict';
 
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+
 var _ = require('lodash');
 var Q = require('q');
 var AWS = require('aws-sdk');
-var gulp = require('gulp');
 var awspublish = require('gulp-awspublish');
 var parallelize = require("concurrent-transform");
 var minimist = require('minimist');
@@ -24,8 +26,8 @@ var init = function() {
     key = Config.isOnProduction ?  Config.credentials.s3.prod.key : Config.credentials.s3.dev.key;
     secret = Config.isOnProduction ?  Config.credentials.s3.prod.secret : Config.credentials.s3.dev.secret;
     region = Config.credentials.s3.region;
-    bucketName = Config.isOnTravisAndMaster ? 'secdisclosures' : 'secdisclosures-' + buildId;
-    console.log('Bucket Name: ' + bucketName);
+    bucketName = Config.isOnProduction ? 'secdisclosures' : 'secdisclosures-' + buildId;
+    $.util.log('Bucket Name: ' + bucketName);
     config = {
         accessKeyId: key,
         secretAccessKey: secret,
@@ -48,12 +50,12 @@ var makeBucketWebsite = function() {
             WebsiteConfiguration : Config.credentials.s3.website
         }, function(err) {
             if (err) {
-                console.log('putBucketWebsite(' + bucketName + '): ' + err);
+                $.util.log('putBucketWebsite(' + bucketName + '): ' + err);
                 defered.reject();
             }
             else {
-                console.log('putBucketWebsite(' + bucketName + ')');
-                console.log('Website available at http://' + bucketName + '.s3-website-' + region + '.amazonaws.com/');
+                $.util.log('putBucketWebsite(' + bucketName + ')');
+                $.util.log('Website available at http://' + bucketName + '.s3-website-' + region + '.amazonaws.com/');
                 defered.resolve();
             }
         }
@@ -104,10 +106,10 @@ var createBucket = function() {
         ACL : 'public-read'
     }, function(err, data) {
         if (err || data === null) {
-            console.error(bucketName + err);
+            $.util.log(bucketName + err);
             defered.reject();
         } else {
-            console.log('createBucket(' + bucketName + ')');
+            $.util.log('createBucket(' + bucketName + ')');
             defered.resolve();
         }
     });
@@ -117,7 +119,7 @@ var createBucket = function() {
 var deleteBucket = function(idempotent) {
     return listObjects(idempotent)
         .then(function(list){
-            console.log('listObjects(' + bucketName + ')');
+            $.util.log('listObjects(' + bucketName + ')');
             if(list && list.length > 0) {
                 var deleteList = [];
                 list.forEach(function(object){
@@ -134,10 +136,10 @@ var deleteBucket = function(idempotent) {
                     }
                 }, function(err, data){
                     if (err || data === null) {
-                        console.error('deleteObjects(' + bucketName + '): ' + err);
+                        $.util.log('deleteObjects(' + bucketName + '): ' + err);
                         defered.reject();
                     } else {
-                        console.log('deleteObjects(' + bucketName + ')');
+                        $.util.log('deleteObjects(' + bucketName + ')');
                         defered.resolve();
                     }
                 });
@@ -150,10 +152,10 @@ var deleteBucket = function(idempotent) {
                 Bucket : bucketName
             }, function(err, data) {
                 if ((err || data === null) && !idempotent) {
-                    console.error('deleteBucket(' + bucketName + '): ' + err);
+                    $.util.log('deleteBucket(' + bucketName + '): ' + err);
                     defered.reject();
                 } else {
-                    console.log('deleteBucket(' + bucketName + ')');
+                    $.util.log('deleteBucket(' + bucketName + ')');
                     defered.resolve();
                 }
             });
@@ -175,8 +177,8 @@ gulp.task('s3-setup', function() {
                     .pipe(awspublish.reporter());
         })
         .catch(function(error){
-            console.error('Error while doing the s3 setup');
-            console.error(error);
+            $.util.log('Error while doing the s3 setup');
+            $.util.log(error);
         });
     } else {
         return gulp.src('dist/**/*')
@@ -193,11 +195,11 @@ gulp.task('s3-teardown', function(done) {
             done();
         })
         .catch(function(error){
-            console.error('Error while doing the s3 setup');
-            console.error(error);
+            $.util.log('Error while doing the s3 setup');
+            $.util.log(error);
         });
     } else {
-        console.log('We are on master, no teardown.');
+        $.util.log('We are on master, no teardown.');
         done();
     }
 });
